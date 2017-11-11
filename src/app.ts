e98a0661bbb28dacc663385d4df56525ab31ebfe
Chris Gibson <cgibson@mrvoxel.com>
2017-11-11 10:54:03 -0800

    Added logic to move the camera around

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,25 @@
 class SimpleGame {
 
     constructor() {
-        this.game = new Phaser.Game(800, 640, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
+        // Construct the game object with a bunch of useful options
+        this.game = new Phaser.Game(
+            800,
+            640,
+            Phaser.AUTO,
+            'content',
+            {
+                preload: this.preload,
+                create: this.create,
+                render: this.render,
+                update: this.update,
+            }
+        );
     }
 
     game: Phaser.Game;
     map: Phaser.Tilemap;
     groundLayer: Phaser.TilemapLayer;
+    cursors: Phaser.CursorKeys;
 
     preload() {
         // Load the level. Down the line we'll want to replace this with a procedural step
@@ -17,7 +30,6 @@ class SimpleGame {
     }
 
     create() {
-
         // Create a new tilemap based on the loaded level
         this.map = this.game.add.tilemap('test_level');
 
@@ -27,8 +39,29 @@ class SimpleGame {
         // Create the ground layer
         this.groundLayer = this.map.createLayer('GroundLayer');
         this.groundLayer.resizeWorld();
+
+        this.cursors = this.game.input.keyboard.createCursorKeys();
     }
 
+    update() {
+        // Move the camera using the arrow keys
+        if (this.cursors.up.isDown) {
+            this.game.camera.y -= 4;
+        } else if (this.cursors.down.isDown) {
+            this.game.camera.y += 4;
+        }
+
+        if (this.cursors.left.isDown) {
+            this.game.camera.x -= 4;
+        } else if (this.cursors.right.isDown) {
+            this.game.camera.x += 4;
+        }
+    }
+
+    render() {
+        // Render debug camera information to the screen
+        this.game.debug.cameraInfo(this.game.camera, 32, 500);
+    }
 }
 
 window.onload = () => {
